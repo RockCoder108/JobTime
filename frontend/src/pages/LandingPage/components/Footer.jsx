7827a8802b860920819d2e936eba3f558bdac315
RockCoder108 <samyak80jain@gmail.com>
2025-09-14 17:05:31 +0530

    Add LandingPage components and remove path utility files

--- frontend/src/pages/LandingPage/components/Footer.jsx
+++ frontend/src/pages/LandingPage/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const data = () => {
+const Footer = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const data = () => {
   )
 }
 
-export default data
+export default Footer
