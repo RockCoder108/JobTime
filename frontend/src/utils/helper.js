209b4771d8b12bad62c908075d9935fd4b3113b5
RockCoder108 <samyak80jain@gmail.com>
2025-09-15 10:50:11 +0530

    add sign

--- frontend/src/utils/helper.js
+++ frontend/src/utils/helper.js
@@ -1,11 +1,5 @@
-import React from 'react'
-
-const helper = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
-
-export default helper
+export const validateEmail = (email) => {
+  if (!email.trim()) return "Email is required";
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) return "Please enter a vaild email address";
+};
