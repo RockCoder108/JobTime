3b8702d44d8435a4149d8721eebff732b1f4b18a
RockCoder108 <samyak80jain@gmail.com>
2025-11-06 10:25:15 +0530

    Update CORS origin to the new production URL in server configuration

--- backend/server.js
+++ backend/server.js
@@ -24,11 +24,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({
-    origin: "https://job-time.vercel.app", // Adjust this in production to your frontend URL
+app.use(
+  cors({
+    origin: "https://job-time-fi7s.vercel.app", // Adjust this in production to your frontend URL
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
-}));
+  })
+);
 app.use(express.json());
 
 // Routes
