176a07ae55543baa3f4fd937165ff4126dc4bbf0
RockCoder108 <samyak80jain@gmail.com>
2025-11-10 00:08:27 +0530

    Refactor SignUp component to use Link for navigation instead of anchor tags

--- frontend/src/pages/Auth/SignUp.jsx
+++ frontend/src/pages/Auth/SignUp.jsx
@@ -430,12 +430,12 @@ const SignUp = () => {
           <div className="text-center">
             <p className="text-gray-600">
               Already have an account?{" "}
-              <a
-                href="/login"
+              <Link
+                to="/login"
                 className="text-blue-600 hover:text-blue-700 font-medium"
               >
                 Sign in here
-              </a>
+              </Link>
             </p>
           </div>
         </form>
